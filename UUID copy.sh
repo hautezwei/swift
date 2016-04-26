53465f4ba9cf617fe8dcbe3c41cdce292c57e01c
DenisZen <charlesdavidwince@gmail.com>
2016-04-26 18:04:55 +0800

    add lines

--- UUID copy.sh
+++ UUID copy.sh
@@ -7,3 +7,4 @@ for f in ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/*;
     do defaults write "$f/Contents/Info" DVTPlugInCompatibilityUUIDs -array-add $XCODEUUID;
     done
 #  this is a shell script to let you change the UUID
+# add some special moments to go to China  for interview spec 
